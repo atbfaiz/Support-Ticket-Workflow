6f6c19a5a058db6f5ad1dbc5a9687247a478bd73
atbfaiz <109590289+atbfaiz@users.noreply.github.com>
2025-01-28 11:47:43 +0000

    added details

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ if "df" not in st.session_state:
     data = {
         "ID": [f"TICKET-{i}" for i in range(1100, 1000, -1)],
         "Issue": np.random.choice(issue_descriptions, size=100),
-        "Status": np.random.choice(["Open", "In Progress", "Closed"], size=100),
+        "Status": np.random.choice(["Ghosted", "No Response", "Fake"], size=100),
         "Priority": np.random.choice(["High", "Medium", "Low"], size=100),
         "Date Submitted": [
             datetime.date(2023, 6, 1) + datetime.timedelta(days=random.randint(0, 182))
